0a89be9e6c0a41cee4feb2d43acf17a68241fe9a
Erik Zscheile <erik.zscheile.ytrizja@gmail.com>
2018-08-14 19:49:53 +0200

    try to fix hash.hpp (2)

--- src/hash.hpp
+++ src/hash.hpp
@@ -5,12 +5,5 @@
 #pragma once
 #include <zs/ll/hash.hpp>
 namespace zsparsell {
-  template<class T>
-  using hash_combine = llzs::hash_combine<T>;
-
-  template<typename T, typename... Types>
-  using hash_combine = llzs::hash_combine<T, Types...>;
-
-  template<typename... Types>
-  using hash_val     = llzs::hash_val<Types...>;
+  using namespace llzs;
 }
