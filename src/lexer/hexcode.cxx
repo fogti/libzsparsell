6468cf16030309a662a1a30ed277a3984898761b
Erik Zscheile <erik.zscheile.ytrizja@gmail.com>
2018-10-09 21:16:05 +0200

    fix decode_hexnum

--- src/lexer/hexcode.cxx
+++ src/lexer/hexcode.cxx
@@ -9,25 +9,23 @@ namespace zsparsell {
 // disable warning about case X ... Y
 #pragma GCC diagnostic ignored "-Wpedantic"
 bool decode_hexnum(const char what, uint8_t &ret) noexcept {
+  char dif;
   switch(what) {
     case '0' ... '9':
-      // as usual
-      ret = what - '0';
-      break;
+      dif = '0'; break;
 
-    case 'A' ... 'A':
-      ret = 10 + (what - 'A');
-      break;
+    case 'A' ... 'F':
+      dif = '7'; break;
 
     case 'a' ... 'f':
-      ret = 10 + (what - 'a');
-      break;
+      dif = 'W'; break;
 
     default:
       ret = 0;
       return false;
   }
 
+  ret = what - dif;
   return true;
 }
 
